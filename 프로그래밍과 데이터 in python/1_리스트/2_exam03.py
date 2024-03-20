54ea139b7395125e0033523c163121301aaac8e0
Yoon1029 <tomato3750@naver.com>
2024-03-20 20:49:41 +0900

    1_리스트

---
+++ 프로그래밍과 데이터 in python/1_리스트/2_exam03.py
@@ -0,0 +1,35 @@
+# 원화(￦)에서 달러($)로 변환하는 함수
+def krw_to_usd(krw):
+    # 여기에 코드를 작성하세요
+    usd = krw / 1000
+    return usd
+
+# 달러($)에서 엔화(￥)로 변환하는 함수
+def usd_to_jpy(usd):
+    # 여기에 코드를 작성하세요
+    jpy = usd / 8 * 1000
+    return jpy
+
+# 원화(￦)으로 각각 얼마인가요?
+prices = [34000, 13000, 5000, 21000, 1000, 2000, 8000, 3000]
+print("한국 화폐: " + str(prices))
+
+# prices를 원화(￦)에서 달러($)로 변환하기
+# 여기에 코드를 작성하세요
+i = 0
+while i < len(prices):
+    prices[i] = krw_to_usd(prices[i])
+    i += 1
+
+# 달러($)로 각각 얼마인가요?
+print("미국 화폐: " + str(prices))
+
+# prices를 달러($)에서 엔화(￥)으로 변환하기
+# 여기에 코드를 작성하세요
+i = 0
+while i < len(prices):
+    prices[i] = usd_to_jpy(prices[i])
+    i += 1
+
+# 엔화(￥)으로 각각 얼마인가요?
+print("일본 화폐: " + str(prices))
